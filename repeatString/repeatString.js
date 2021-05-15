2073e0f539bdf6fe38e6cb1756f0a88a0254b855
narcisooliveira <nrcsfo@gmail.com>
2021-05-14 21:28:00 -0300

    update repeatString function

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,26 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    var result = string
+    if (times === 3){
+        for (var i = 1; i < times; i++){
+            result += string
+        }
+        return result 
+    }
+    else if (times >= 10){
+        for (var i = 1; i < times; i++){
+            result += string
+        }
+        return result  
+    }
+    else if (times === 1){
+        return result
+    }else if (times === 0){
+        return ''
+    }else if (times === -1){
+        return 'ERROR'
+    }else if (string === ''){
+        return ''
+    }
 }
 
-module.exports = repeatString
+module.exports = repeatString('hey', 3)
