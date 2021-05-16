bf34530bafcf2130cdf5d8c4aaa55d90b32c0dff
narcisooliveira <nrcsfo@gmail.com>
2021-05-16 11:32:00 -0300

    update reverseString file and testing

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    var reverseWord = '';
+    for (let index = word.length -1; index >= 0; index--) {
+        reverseWord += word[index];
+    }return reverseWord;
 }
 
 module.exports = reverseString
