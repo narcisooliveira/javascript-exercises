c63e9e636f3798c239ce3c9f578ac6b5f19a6eaf
narcisooliveira <nrcsfo@gmail.com>
2021-05-17 21:48:09 -0300

    update leapYears file and testing

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    yearTest = years
+    if (yearTest % 4 === 0) {
+        if (yearTest % 100 === 0) {
+            if (yearTest % 400 === 0) {
+                return true
+            }
+            return false
+        }
+        return true
+    }return false
 }
 
 module.exports = leapYears
