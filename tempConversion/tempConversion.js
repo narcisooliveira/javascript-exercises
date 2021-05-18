ecb2042625f0a84c3bae0ce0f4ddf519e57c5da2
narcisooliveira <nrcsfo@gmail.com>
2021-05-17 21:45:37 -0300

    update testint files

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let aux = temp - 32
+  tempConverted = aux / 1.8 
+  return parseFloat(tempConverted.toFixed(1))
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    let aux = temp * 1.8
+    tempConverted = aux + 32
+    return parseFloat(tempConverted.toFixed(1))
 }
 
 module.exports = {
